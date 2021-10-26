744dd98f82150ed18ed97d3305c86221e236bdc0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-26 20:56:33 +0800

    Create setup.r

---
+++ setup.r
@@ -0,0 +1,39 @@
+# R语言安装包
+
+
+
+
+
+install.packages("BiocManager")
+install.packages("colorspace")
+install.packages("stringi")
+install.packages("ggplot2")
+install.packages("gplots")
+install.packages("ggpubr")
+install.packages("ggpmisc")
+install.packages("GOplot")
+install.packages("digest")
+install.packages("stringr")
+install.packages("survival")
+install.packages("survminer")
+install.packages("survivalROC")
+install.packages("beeswarm")
+install.packages("xlsx")
+BiocManager::install("DOSE"             ,update=TRUE, ask=FALSE)
+BiocManager::install("clusterProfiler"  ,update=TRUE, ask=FALSE)
+BiocManager::install("enrichplot"       ,update=TRUE, ask=FALSE)
+BiocManager::install("org.Hs.eg.db"     ,update=TRUE, ask=FALSE)
+BiocManager::install("org.Mm.eg.db"     ,update=TRUE, ask=FALSE)
+BiocManager::install("impute"           ,update=TRUE, ask=FALSE)
+BiocManager::install("limma"            ,update=TRUE, ask=FALSE)
+BiocManager::install("biomaRt"          ,update=TRUE, ask=FALSE)
+BiocManager::install("edgeR"            ,update=TRUE, ask=FALSE)
+BiocManager::install("DESeq"            ,update=TRUE, ask=FALSE)
+BiocManager::install("mirna40cdf"       ,update=TRUE, ask=FALSE)
+BiocManager::install("affy"             ,update=TRUE, ask=FALSE)
+BiocManager::install("tcltk"            ,update=TRUE, ask=FALSE)
+BiocManager::install("ChIPseeker"       ,update=TRUE, ask=FALSE)
+
+
+
+
