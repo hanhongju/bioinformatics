9a764c3a4fac243bceb43cf5abeb1f9a6e80e2d3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-11-06 20:39:51 +0800

    Create impute.r

---
+++ impute.r
@@ -0,0 +1,14 @@
+#impute and normalize data
+library(impute)
+library(limma)
+setwd("D:\\GSE9508\\cptac\\04.normalize") 
+readtable=read.csv(file,header=T,check.names=F,row.names=1)
+readtable=as.matrix(readtable)
+imputedmatrix=impute.knn(readtable)
+readtable=imputedmatrix$data
+#normalize
+normalizedData=normalizeBetweenArrays(as.matrix(readtable))
+normalizedData=rbind(geneNames=colnames(normalizedData),normalizedData)
+write.table(normalizedData,file="normalize.txt",sep="\t",quote=F,col.names=F)
+
+
