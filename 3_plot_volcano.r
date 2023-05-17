19518c2713962326a658b57fe28b23042bdccab2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-17 23:53:50 +0800

    Update 3_plot_volcano.r

--- 3_plot_volcano.r
+++ 3_plot_volcano.r
@@ -1,40 +1,33 @@
-#volcano plot
+
+
 library('gplots')
 
 
-pdf(file="volcano.pdf",width = 8,height = 8)
-xMax=5
-yMax=5
-allDiff=all
-plot(allDiff$logFC,  -log10(allDiff$FDR),
-     xlab = "lg(FC)", ylab = "-lg(FDR)", main = "St2-/- vs WT volcano plot",
-     xlim = c(-xMax,xMax),  ylim = c(0,yMax),
+workdir = r"(C:\Users\hj\Desktop\GSE209841)"
+setwd(workdir)
+inputFile = "alldiffDEG.csv"
+
+
+data=read.csv(inputFile,header = T,row.names = 1)
+uppoints   =data[data$pValue  <  0.05   &   data$logFC  >  1, ]
+downpoints =data[data$pValue  <  0.05   &   data$logFC  < -1, ]
+
+
+
+
+pdf(file="DEGvolcano.pdf",width = 8,height = 8)
+xMax=500
+yMax=6
+plot(data$logFC,  -log10(data$pValue),
+     xlab = "lg(FC)", ylab = "-lg(pValue)",
+     main = "Spinal Cord Injury VS Naive",
+     xlim = c(-xMax,xMax),  ylim = c(1,yMax),
      yaxs="i",  pch=20, cex=1)
-uppoints   =allDiff[allDiff$FDR  <  0.05   &   allDiff$logFC  >  1, ]
-downpoints =allDiff[allDiff$FDR  <  0.05   &   allDiff$logFC  < -1, ]
-ifng=all[rownames(all) == "Ifng", ]
-points(uppoints$logFC     ,-log10(uppoints$FDR)     ,pch=20   ,col="red"    ,cex=1)
-points(downpoints$logFC   ,-log10(downpoints$FDR)   ,pch=20   ,col="green"  ,cex=1)
-points(ifng$logFC, -log10(ifng$FDR), pch=20,col="chocolate", cex=1)
-abline(h=-log10(0.05)   ,lty=2   ,lwd=1)
-abline(v=1   ,lty=2   ,lwd=1)
-abline(v=-1  ,lty=2   ,lwd=1)
-dev.off()
 
-
-
-
-diffsig    =  all[all$FDR  < 0.05   & abs(all$logFC) >  1        ,]
-diffup     =  all[all$FDR  < 0.05   &     all$logFC  >  1        ,]
-diffdown   =  all[all$FDR  < 0.05   &     all$logFC  < -1        ,]
-diffbigSig  =  diffsig[(diffsig$FDR < 0.05 & (diffsig$logFC >  3 |  diffsig$logFC< -3)) , ]
-heatmapData  =  newData[rownames(diffbigSig),]
-hmExp=log10(heatmapData+0.001)
-hmMat=as.matrix(hmExp)
-colnames(hmMat) = group
-pdf(file="heatmap.pdf",width = 10,height = 10)
-heatmap.2(hmMat, col = 'greenred', trace="none"
-          ,cexCol = 1    ,cexRow = 1  )
+points(uppoints$logFC     ,-log10(uppoints$pValue)     ,pch=20   ,col="red"          ,cex=1)
+points(downpoints$logFC   ,-log10(downpoints$pValue)   ,pch=20   ,col="aquamarine4"  ,cex=1)
+abline(v=0   ,lty=2   ,lwd=1)
+abline(h=1.2   ,lty=2   ,lwd=1)
 dev.off()
 
 
