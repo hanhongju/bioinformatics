08983508d00ebd9c522891685c4b9a50fc22035d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-25 18:02:22 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -1,16 +1,21 @@
 #安装软件包
 #要用管理员权限在CMD中安装，不能在RStiodio运行
+
+
+
+
 pkgs = c("BiocManager", "colorspace", "stringi", "ggplot2", "gplots", "ggpubr", "ggpmisc", "GOplot", "digest", "stringr",
          "rms", "foreign", "survival", "survminer", "survivalROC", "beeswarm", "caret", "plotly", "gower", "xfun",
-         "dplyr", "org.Hs.eg.db", "org.Mm.eg.db", "utf8", "ipred", "checkmate", "igraph")
+         "dplyr", "utf8", "ipred", "checkmate", "igraph", "tcltk")
 lapply(pkgs, require, character.only = TRUE)
-install.packages(pkgs, type = "source", repo = "https://mirrors.sustech.edu.cn/CRAN/")
+install.packages(pkgs, repo = "https://mirrors.sustech.edu.cn/CRAN/")
 
 
-pkgs = c("DOSE", "clusterProfiler", "enrichplot", "org.Hs.eg.db", "org.Mm.eg.db", "impute", "limma", "biomaRt", "edgeR", "affy",
-         "tcltk", "ChIPseeker", "ReactomePA")
-BiocManager::install(pkgs, update = TRUE, ask = FALSE, type = "source", repo = "https://mirrors.sustech.edu.cn/CRAN/")
 
 
-lapply(pkgs, require, character.only = TRUE)
-install.packages(pkgs, type = "win.binary", repo = "https://mirrors.sustech.edu.cn/CRAN/")
+pkgs = c("DOSE", "clusterProfiler", "enrichplot", "org.Hs.eg.db", "org.Mm.eg.db", "impute", "limma", "biomaRt", "edgeR", "affy", "ChIPseeker", "ReactomePA")
+BiocManager::install(pkgs, update = TRUE, ask = FALSE, site_repository = "https://mirrors.sustech.edu.cn/CRAN/")
+
+
+
+
