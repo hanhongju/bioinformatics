a678cfba921e6f70da28f9d0ec25062843d2eda9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-25 18:02:44 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -2,15 +2,11 @@
 #要用管理员权限在CMD中安装，不能在RStiodio运行
 
 
-
-
 pkgs = c("BiocManager", "colorspace", "stringi", "ggplot2", "gplots", "ggpubr", "ggpmisc", "GOplot", "digest", "stringr",
          "rms", "foreign", "survival", "survminer", "survivalROC", "beeswarm", "caret", "plotly", "gower", "xfun",
          "dplyr", "utf8", "ipred", "checkmate", "igraph", "tcltk")
 lapply(pkgs, require, character.only = TRUE)
 install.packages(pkgs, repo = "https://mirrors.sustech.edu.cn/CRAN/")
-
-
 
 
 pkgs = c("DOSE", "clusterProfiler", "enrichplot", "org.Hs.eg.db", "org.Mm.eg.db", "impute", "limma", "biomaRt", "edgeR", "affy", "ChIPseeker", "ReactomePA")
