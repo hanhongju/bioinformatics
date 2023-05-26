db5d58f1a048ee0494410a7374ec6ea711593e1c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-26 13:38:36 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -1,9 +1,14 @@
 #安装软件包
 
-pkgs = c("BiocManager", "colorspace", "stringi", "ggplot2", "gplots", "ggpubr", "ggpmisc", "GOplot", "digest", "stringr",
-         "rms", "foreign", "survival", "survminer", "survivalROC", "beeswarm", "caret", "plotly", "gower", "xfun",
-         "dplyr", "utf8", "ipred", "checkmate", "igraph", "tcltk", "DOSE", "clusterProfiler", "enrichplot", "org.Hs.eg.db",
-         "org.Mm.eg.db", "impute", "limma", "biomaRt", "edgeR", "affy", "ChIPseeker", "ReactomePA")
+
+pkgs = c("affy","beeswarm","BiocManager","biomaRt","caret",
+         "checkmate","ChIPseeker","clusterProfiler","colorspace","digest",
+         "DOSE","dplyr","edgeR","enrichplot","foreign",
+         "ggplot2","ggpmisc","ggpubr","GOplot","gower",
+         "gplots","igraph","impute","ipred","limma",
+         "org.Hs.eg.db","org.Mm.eg.db","plotly","ReactomePA","rms",
+         "stringi","stringr","survival","survivalROC","survminer",
+         "tcltk","utf8","xfun")
 lapply(pkgs, require, character.only = TRUE)
 install.packages(pkgs, repo = "https://mirrors.sustech.edu.cn/CRAN/")
 BiocManager::install(pkgs, update = TRUE, ask = FALSE, site_repository = "https://mirrors.sustech.edu.cn/CRAN/")
