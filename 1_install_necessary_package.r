3463a8cffd06cbce5b9e5d7aea6ea38003d62d78
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-06-02 11:38:39 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -1,6 +1,7 @@
 #安装软件包
 
 
+
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
 pkgs = c("affy","beeswarm","BiocManager","biomaRt","caret",
          "checkmate","ChIPseeker","clusterProfiler","colorspace","digest",
@@ -10,12 +11,10 @@ pkgs = c("affy","beeswarm","BiocManager","biomaRt","caret",
          "org.Hs.eg.db","org.Mm.eg.db","plotly","ReactomePA","rms",
          "stringi","stringr","survival","survivalROC","survminer",
          "tcltk","utf8","xfun")
-require("BiocManager")
-install.packages("BiocManager", repo = "https://mirrors.sustech.edu.cn/CRAN/")
-BiocManager::install(pkgs, update = TRUE, ask = FALSE,
+lapply(pkgs, require, character.only = TRUE)
+install.packages(pkgs, repo = "https://mirrors.sustech.edu.cn/CRAN/")
+BiocManager::install(pkgs, update = FALSE,
                      site_repository = "https://mirrors.sustech.edu.cn/CRAN/")
-
-
 
 
 
