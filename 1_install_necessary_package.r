030bb756ebdcc346718f2f8ba96a73ab7b87e127
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-04 00:24:55 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -2,10 +2,9 @@ pkgs = c("affy","beeswarm","biomaRt","caret","lme4","xml2","pbkrtest","gridtext"
          "checkmate","ChIPseeker","clusterProfiler","colorspace","digest",
          "DOSE","dplyr","edgeR","enrichplot","foreign","ggpubr","ggtext",
          "ggplot2","ggpmisc","ggpubr","GOplot","gower","car","rstatix",
-         "gplots","igraph","impute","ipred","limma",
+         "gplots","igraph","impute","ipred","limma","tcltk","utf8","xfun",
          "org.Hs.eg.db","org.Mm.eg.db","plotly","ReactomePA","rms",
-         "stringi","stringr","survival","survivalROC","survminer",
-         "tcltk","utf8","xfun")
+         "stringi","stringr","survival","survivalROC","survminer")
 install.packages("BiocManager", repo = "https://mirrors.sustech.edu.cn/CRAN/")
 BiocManager::install(pkgs, update = FALSE, site_repository = "https://mirrors.sustech.edu.cn/CRAN/")
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
