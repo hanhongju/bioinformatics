b7b73c3283c1aa797474eeb1fb858d0e5b2220da
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-06 23:41:40 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -1,5 +1,6 @@
 # 在R中安装包前先将系统软件升级到最新：
 # apt -y update && DEBIAN_FRONTEND=noninteractive apt -y full-upgrade && apt -y autoremove
+# shell中 sudo R 后获取管理员权限并开始安装包
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
 .libPaths(Sys.getenv("R_LIBS_USER"))
 options(repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
@@ -20,8 +21,7 @@ lapply(pkgs, require, character.only = TRUE)
 
 
 
-# 安装软件包，个人安装的包在:
-# C:\Users\hj\AppData\Local\R\win-library\4.4 @ Windows
-# /home/hj/R/x86_64-pc-linux-gnu-library/4.4 @ Ubuntu 24 LTS
+# Windows中个人包安装位置在 C:\Users\hj\AppData\Local\R\win-library\4.4
+# Ubuntu中系统包安装在 /usr/lib/R/library/
 # 参考文献：
 # https://github.com/bionoob7/bionoob7.github.io/issues/9
