9642e4af6998d6a1e8fb3a38ec4bb7b5068c0942
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-07 14:55:37 +0800

    Update 1_install_necessary_package.r

--- 1_install_necessary_package.r
+++ 1_install_necessary_package.r
@@ -1,6 +1,7 @@
 # 在R中安装包前先将系统软件升级到最新：
 # apt -y update && DEBIAN_FRONTEND=noninteractive apt -y full-upgrade && apt -y autoremove
 # shell中 sudo R 后获取管理员权限
+Sys.setenv("https_proxy"="socks5://127.0.0.1:8000")
 options(repos = "https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
 options(BioC_mirror = "https://mirrors.tuna.tsinghua.edu.cn/bioconductor/")
 pkgs = c("affy","beeswarm","biomaRt","caret","lme4","xml2","pbkrtest","gridtext",
