4535e0311ca9741b98e45f988080065774308401
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-02 01:13:16 +0800

    Update non_lineager_equation.r

--- non_lineager_equation.r
+++ non_lineager_equation.r
@@ -1,5 +1,3 @@
-library(nleqslv)
-rm(list = ls())
 # coupon payment per period
 pmt = 55
 # future value paid at maturity
@@ -9,15 +7,21 @@ nper = 26
 # Present Value
 pv = 1169
 # y <- yield to first par call 
+# uniroot函数求解非线性方程
+f <- function(y) pmt*((1-1/(1+y)^nper)/y)+fv/(1+y)^nper-pv
+sloveuniroot <- uniroot(f,c(-1,1))
+sloveuniroot$root
+# nleqslv包求解非线性方程（组）
+library(nleqslv)
 equations <- function(x) {
   f1 <- pmt*((1-1/(1+x[1])^nper)/x[1])+fv/(1+x[1])^nper-pv
   return(c(f1))
 }
 slove <- nleqslv(1, equations)
 slove$x
+rm(list = ls())
 
 
 
 
-# nleqslv包求解非线性方程（组）
 # 参考文献https://blog.51cto.com/u_16213380/12522271
