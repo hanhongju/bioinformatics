4e2fe6b8f40ec82330a62a9380a6b3e6a3b0637d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-01 23:54:57 +0800

    Update 1_install_package.r

--- 1_install_package.r
+++ 1_install_package.r
@@ -7,7 +7,7 @@ pkgs = c("affy","beeswarm","biomaRt","car","caret","checkmate","ChIPseeker","clu
          ,"devtools","digest","DOSE","dplyr","dplyr","edgeR","ellipsis","enrichplot","foreign","ggplot2"
          ,"ggpmisc","ggpubr","ggpubr","ggsci","ggtext","GOplot","gower","gplots","gridtext","igraph","impute"
          ,"ipred","limma","lme4","monocle","openxlsx","org.Hs.eg.db","org.Mm.eg.db","pbkrtest","pkgbuild"
-         ,"pkgload","plotly","profvis","ReactomePA","remotes","reticulate","rms","rstatix","rtracklayer"
+         ,"pkgload","plotly","profvis","ReactomePA","remotes","reticulate","rms","rstatix","rtracklayer","BB"
          ,"sessioninfo","Seurat","stringi","stringr","survival","survivalROC","survminer","tcltk","tidyverse"
          ,"urlchecker","usethis","utf8","xfun","xml2")
 # Windows中从源码编译需要安装rtools
