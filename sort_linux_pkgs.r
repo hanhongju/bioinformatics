a88bc9e51a6e2bc59af89234b9b099eb6af3dda4
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-09 09:59:44 +0800

    Update sort_linux_pkgs.r

--- sort_linux_pkgs.r
+++ sort_linux_pkgs.r
@@ -1,5 +1,14 @@
-pkgs = ""
+pkgs = "
+                      brasero build-essential calibre cifs-utils cmake code curl dirmngr \
+                      firefox gimp gparted handbrake hashdeep krita krita-l10n libcurl4-openssl-dev \
+                      libfribidi-dev libharfbuzz-dev libmagick++-dev libpcre3 libpcre3-dev libreoffice \
+                      libreoffice-help-zh-cn libreoffice-l10n-zh-cn libssl-dev net-tools openssl p7zip-full \
+                      qbittorrent r-base rhythmbox samba smplayer software-properties-common steam \
+                      thunderbird trojan usb-creator-gtk virtualbox wget zlib1g-dev
+"
 pkgslist <- strsplit(pkgs, " ")
 pkgsvectorsorted <- sort(pkgslist[[1]])
 pkgssorted <- paste(pkgsvectorsorted, collapse = " ")
 pkgssorted
+
+
