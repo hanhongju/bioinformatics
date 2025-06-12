72758e38df15f6857a4b91ef7eb2b137df6d8b2c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-12 12:05:25 +0800

    Update 1_install_package.r

--- 1_install_package.r
+++ 1_install_package.r
@@ -4,11 +4,11 @@
 options(repos = "https://mirrors.nju.edu.cn/CRAN/")
 options(BioC_mirror="https://mirrors.nju.edu.cn/bioconductor/")
 pkgs = c("affy","beeswarm","biomaRt","car","caret","checkmate","ChIPseeker","clusterProfiler","colorspace"
-         ,"devtools","digest","DOSE","dplyr","dplyr","edgeR","ellipsis","enrichplot","foreign","ggplot2","curl","packrat","rsconnect"
+         ,"devtools","digest","DOSE","dplyr","dplyr","edgeR","ellipsis","enrichplot","ggplot2","curl","packrat","rsconnect"
          ,"ggpmisc","ggpubr","ggpubr","ggsci","ggtext","GOplot","gower","gplots","gridtext","igraph","impute"
          ,"ipred","limma","lme4","monocle","openxlsx","org.Hs.eg.db","org.Mm.eg.db","pbkrtest","pkgbuild"
          ,"pkgload","plotly","profvis","ReactomePA","remotes","reticulate","rms","rstatix","rtracklayer","nleqslv"
-         ,"sessioninfo","Seurat","stringi","stringr","survival","survivalROC","survminer","tcltk","tidyverse"
+         ,"sessioninfo","Seurat","stringi","stringr","survivalROC","survminer","tidyverse"
          ,"urlchecker","usethis","utf8","xfun","xml2")
 # Windows中从源码编译需要安装rtools
 if (!require("BiocManager")) install.packages("BiocManager", INSTALL_opts = "--no-lock")
