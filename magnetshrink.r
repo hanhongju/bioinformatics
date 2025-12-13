5f8da806aa82fa8dfa7490aea761b8b19e7f89ab
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-12-14 03:23:06 +0800

    Update magnetshrink.r

--- magnetshrink.r
+++ magnetshrink.r
@@ -1,7 +1,7 @@
 library(stringr)
 workdir = r"(C:\Users\hj\Desktop)"
 setwd(workdir)
-inputFile = "deletedn.txt"
+inputFile = "todeletedn.txt"
 data <- read.csv(inputFile, header = F)
 data$V1 <- gsub("&.*", "", data$V1)
 write.table(data, file = "nodn.txt",
